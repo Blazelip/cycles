e910a55d081921994967a7fd737dc232dddc9420
Blazelip <filin1994@rambler.ru>
2021-01-18 10:35:42 +0300

    Делает секцию каталога и видео

--- gulpfile.js
+++ gulpfile.js
@@ -112,5 +112,5 @@ gulp.task('clean', function () {
   return del('build');
 });
 
-gulp.task('build', gulp.series('clean', 'copy', 'css', 'javascript', 'sprite', 'html'));
+gulp.task('build', gulp.series('clean', 'images', 'webp', 'copy', 'css', 'javascript', 'sprite', 'html'));
 gulp.task('start', gulp.series('build', 'server'));
